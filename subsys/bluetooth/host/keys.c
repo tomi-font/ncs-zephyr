a0f06b4774bd616e669285a51b44b1eb9bb2b0cc
Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
2025-06-10 23:20:32 +0200

    [nrf noup] bluetooth: host: Add support for bonding with same peer
    This commit adds a new Kconfig option by enabling which Host will keep
    bonding with the same Central instead of rejecting pairing.
    
    Brief implementation details:
    
    This implementation adds a new flag to bt_keys struct:
    BT_KEYS_ID_CONFLICT. The flag is set, when:
    - bonding with the same peer and conflict identified
    - when loading conflicting keys from persistent storage.
    
    When bonding and conflict is identified, the new keys aren't added to
    the Resolving List immediately. Instead, the old keys stay in the
    Resolving List.
    
    When start advertising, Host finds conflicting keys that are already
    added to the Resolving List and substitues them.
    
    If, however, there is another advertiser already started for the added
    keys, the new request is reject and advertising start function returns
    -EPERM.
    
    This is supported by Peripheral role only for now.
    
    Allow to use CONFIG_BT_ID_AUTO_SWAP_MATCHING_BONDS Kconfig option even
    if CONFIG_BT_PRIVACY is disabled.
    
    This is because CONFIG_BT_PRIVACY configures privacy of local device
    will still allows to resolve peer address. During pairing, peer device
    may send its Identity Address and IRK which then can be used for address
    resolution. This doesn't require CONFIG_BT_PRIVACY be enabled.
    
    Signed-off-by: Pavel Vasilyev <pavel.vasilyev@nordicsemi.no>
    (cherry picked from commit 63e52b8a603485ca7d5cbaf6ec9633aa93001b5f)

--- subsys/bluetooth/host/keys.c
+++ subsys/bluetooth/host/keys.c
@@ -326,12 +326,45 @@ void bt_keys_add_type(struct bt_keys *keys, enum bt_keys_type type)
 
 void bt_keys_clear(struct bt_keys *keys)
 {
+	struct bt_keys *conflict = NULL;
+
 	__ASSERT_NO_MSG(keys != NULL);
 
 	LOG_DBG("%s (keys 0x%04x)", bt_addr_le_str(&keys->addr), keys->keys);
 
+	if (IS_ENABLED(CONFIG_BT_ID_AUTO_SWAP_MATCHING_BONDS) &&
+	    (keys->flags & BT_KEYS_ID_CONFLICT) != 0) {
+		/* We need to check how many conflicting keys left. If there is only one conflicting
+		 * key left, we can remove the BT_KEYS_ID_CONFLICT flag from it so that Host don't
+		 * need to check and update the Resolving List whenever this is needed. The key
+		 * should be re-added to the Resolving List.
+		 */
+		bool found_multiple;
+
+		found_multiple = bt_id_find_conflict_multiple(keys, &conflict);
+		if (conflict) {
+			if (found_multiple || (conflict->state & BT_KEYS_ID_ADDED) != 0) {
+				/* If we found multiple conflicting keys or the conflicting key
+				 * is already added to the ID list, we don't need to clear the
+				 * conflict flag for it and re-add it to the Resolving List.
+				 */
+				conflict = NULL;
+			} else {
+				/* Clear the conflict flag for the conflicting key */
+				conflict->flags &= ~BT_KEYS_ID_CONFLICT;
+			}
+		}
+	}
+
 	if (keys->state & BT_KEYS_ID_ADDED) {
 		bt_id_del(keys);
+	}
+
+	if (conflict) {
+		/* Re-add the conflicting key to the Resolving List if it was the last conflicting
+		 * key.
+		 */
+		bt_id_pending_keys_update_set(conflict, BT_KEYS_ID_PENDING_ADD);
 	}
 
 	if (IS_ENABLED(CONFIG_BT_SETTINGS)) {
@@ -359,6 +392,28 @@ int bt_keys_store(struct bt_keys *keys)
 	LOG_DBG("Stored keys for %s", bt_addr_le_str(&keys->addr));
 
 	return 0;
+}
+
+static void check_and_set_id_conflict_flag(struct bt_keys *keys)
+{
+	struct bt_keys *conflict;
+
+	if (!IS_ENABLED(CONFIG_BT_ID_AUTO_SWAP_MATCHING_BONDS)) {
+		/* If auto-swap is not enabled, we don't need to check for conflicts */
+		return;
+	}
+
+	/* Use bt_id_find_conflict() to check if there are any conflicting keys for the given keys.
+	 * If there is at least one, set the BT_KEYS_ID_CONFLICT flag for both the keys and the
+	 * conflicting key.
+	 */
+	conflict = bt_id_find_conflict(keys, true);
+	if (conflict != NULL) {
+		LOG_DBG("Found conflicting key %p.", conflict);
+
+		keys->flags |= BT_KEYS_ID_CONFLICT;
+		conflict->flags |= BT_KEYS_ID_CONFLICT;
+	}
 }
 
 static int keys_set(const char *name, size_t len_rd, settings_read_cb read_cb,
@@ -475,6 +530,8 @@ static int keys_set(const char *name, size_t len_rd, settings_read_cb read_cb,
 		keys->flags &= ~BT_KEYS_AUTHENTICATED;
 	}
 
+	check_and_set_id_conflict_flag(keys);
+
 	LOG_DBG("Successfully restored keys for %s", bt_addr_le_str(&addr));
 #if defined(CONFIG_BT_KEYS_OVERWRITE_OLDEST)
 	if (aging_counter_val < keys->aging_counter) {
@@ -487,6 +544,13 @@ static int keys_set(const char *name, size_t len_rd, settings_read_cb read_cb,
 static void id_add(struct bt_keys *keys, void *user_data)
 {
 	__ASSERT_NO_MSG(keys != NULL);
+
+	if (keys->flags & BT_KEYS_ID_CONFLICT) {
+		/* If the keys have the conflict flag set, we don't want to add them to the ID list,
+		 * as this will cause issues with resolving list.
+		 */
+		return;
+	}
 
 	/* Only mark as pending here. Keys are added to the controller RL on
 	 * demand when advertising or scanning starts, avoiding bt_id_add()
