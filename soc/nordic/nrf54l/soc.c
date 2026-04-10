43335794fce4d5c6d3753fe83cdbc3d2c44e04c4
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2026-04-10 12:24:44 +0200

    Revert "[nrf fromtree] soc: timer: nrf: patch init order of grtc and nrf54l"
    This reverts commit bdd103843fb1500b84a9939916be6fca320d0abd.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- soc/nordic/nrf54l/soc.c
+++ soc/nordic/nrf54l/soc.c
@@ -165,5 +165,4 @@ void arch_busy_wait(uint32_t time_us)
 	nrfx_coredep_delay_us(time_us);
 }
 
-/* Init must precede sys_clock_driver_init */
-SYS_INIT(nordicsemi_nrf54l_init, EARLY, 0);
+SYS_INIT(nordicsemi_nrf54l_init, PRE_KERNEL_1, 0);
