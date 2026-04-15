473fba7d783c9345823028c2417d074a2e2240b1
Andrzej Głąbek <andrzej.glabek@nordicsemi.no>
2026-04-15 12:02:12 +0200

    Revert "[nrf fromtree] usb: device_next: remove udc_buf_get_all()"
    This reverts commit 08ab18336c322aacae4c781edbeb6f53f3fc3e28.
    
    Signed-off-by: Andrzej Głąbek <andrzej.glabek@nordicsemi.no>

--- drivers/usb/udc/udc_renesas_ra.c
+++ drivers/usb/udc/udc_renesas_ra.c
@@ -322,10 +322,14 @@ static int udc_renesas_ra_ep_dequeue(const struct device *dev, struct udc_ep_con
 {
 	struct udc_renesas_ra_data *data = udc_get_private(dev);
 	unsigned int lock_key;
+	struct net_buf *buf;
 
 	lock_key = irq_lock();
 
-	udc_ep_cancel_queued(dev, cfg);
+	buf = udc_buf_get_all(cfg);
+	if (buf != NULL) {
+		udc_submit_ep_event(dev, buf, -ECONNABORTED);
+	}
 
 	if (FSP_SUCCESS != R_USBD_XferAbort(&data->udc, cfg->addr)) {
 		return -EIO;
