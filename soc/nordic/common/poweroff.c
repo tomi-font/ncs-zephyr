nrfconnect/sdk-zephyr

[nrf noup] soc: nordic: Support TF-M for poweroff
Enable going to system off when the TF-M API is available. This
calls the relevant TF-M platform API to enter system off mode using
TF-M. This is a noup because the TF-M service for system off is
currently located in sdk-nrf only.

This is meant to be a short lived commit, work is already ongoing
to upstream an official TF-M system off solution that can be be
added to upstream Zephyr.

Signed-off-by: Georgios Vasilakis <georgios.vasilakis@nordicsemi.no>

soc/nordic/common/poweroff.c
@@ -7,7 +7,9 @@
 #include <zephyr/toolchain.h>
 #include <zephyr/drivers/retained_mem/nrf_retained_mem.h>
 
-#if defined(CONFIG_SOC_SERIES_NRF51X) || defined(CONFIG_SOC_SERIES_NRF52X)
+#if defined(CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE)
+#include "tfm_platform_api.h"
+#elif defined(CONFIG_SOC_SERIES_NRF51X) || defined(CONFIG_SOC_SERIES_NRF52X)
 #include <hal/nrf_power.h>
 #elif defined(CONFIG_SOC_SERIES_NRF54HX)
 #include <power.h>
@@ -30,6 +32,10 @@
 
 void z_sys_poweroff(void)
 {
+#if defined(CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE)
+	tfm_platform_system_off();
+#else
+
 #if defined(CONFIG_HAS_NORDIC_RAM_CTRL)
 	uint8_t *ram_start;
 	size_t ram_size;
@@ -77,6 +83,8 @@ void z_sys_poweroff(void)
 #else
 	nrf_regulators_system_off(NRF_REGULATORS);
 #endif
+
+#endif /* CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE */
 
 	CODE_UNREACHABLE;
 }

tomi-font/ncs-zephyr

(cherry picked from commit 1ccd687ded0cc174e6329fbaf5dda7af5f489a2c)

soc/nordic/common/poweroff.c
@@ -7,7 +7,9 @@
 #include <zephyr/toolchain.h>
 #include <zephyr/drivers/retained_mem/nrf_retained_mem.h>
 
-#if defined(CONFIG_SOC_SERIES_NRF51) || defined(CONFIG_SOC_SERIES_NRF52)
+#if defined(CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE)
+#include "tfm_platform_api.h"
+#elif defined(CONFIG_SOC_SERIES_NRF51) || defined(CONFIG_SOC_SERIES_NRF52)
 #include <hal/nrf_power.h>
 #elif defined(CONFIG_NRF_PLATFORM_HALTIUM)
 #include <haltium_power.h>
@@ -30,6 +32,10 @@
 
 void z_sys_poweroff(void)
 {
+#if defined(CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE)
+	tfm_platform_system_off();
+#else
+
 #if defined(CONFIG_HAS_NORDIC_RAM_CTRL)
 	uint8_t *ram_start;
 	size_t ram_size;
@@ -77,6 +83,8 @@ void z_sys_poweroff(void)
 #else
 	nrf_regulators_system_off(NRF_REGULATORS);
 #endif
+
+#endif /* CONFIG_TFM_NRF_SYSTEM_OFF_SERVICE */
 
 	CODE_UNREACHABLE;
 }