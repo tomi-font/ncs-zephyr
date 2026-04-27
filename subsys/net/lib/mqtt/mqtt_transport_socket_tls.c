nrfconnect/sdk-zephyr

[nrf noup] net: mqtt: add native TLS support
This commit adds an extra parameter in the configuration
structure to configure native TLS support at runtime.

Signed-off-by: Mirko Covizzi <mirko.covizzi@nordicsemi.no>

subsys/net/lib/mqtt/mqtt_transport_socket_tls.c
@@ -22,10 +22,15 @@ int mqtt_client_tls_connect(struct mqtt_client *client)
 {
 	const struct sockaddr *broker = client->broker;
 	struct mqtt_sec_config *tls_config = &client->transport.tls.config;
+	int type = SOCK_STREAM;
 	int ret;
 
+	if (tls_config->set_native_tls) {
+		type |= SOCK_NATIVE_TLS;
+	}
+
 	client->transport.tls.sock = zsock_socket(broker->sa_family,
-						  SOCK_STREAM, IPPROTO_TLS_1_2);
+						  type, IPPROTO_TLS_1_2);
 	if (client->transport.tls.sock < 0) {
 		return -errno;
 	}

tomi-font/ncs-zephyr

Make MQTT `set_native_tls` option work w/o socket dispatcher enabled.
Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>
(cherry picked from commit 8025e2ec04a513acfae96d0561486b0d278ec53b)

subsys/net/lib/mqtt/mqtt_transport_socket_tls.c
@@ -23,10 +23,15 @@ int mqtt_client_tls_connect(struct mqtt_client *client)
 {
 	const struct net_sockaddr *broker = client->broker;
 	struct mqtt_sec_config *tls_config = &client->transport.tls.config;
+	int type = NET_SOCK_STREAM;
 	int ret;
 
+	if (!IS_ENABLED(CONFIG_NET_SOCKETS_OFFLOAD_DISPATCHER) && tls_config->set_native_tls) {
+		type |= SOCK_NATIVE_TLS;
+	}
+
 	client->transport.tls.sock = zsock_socket(broker->sa_family,
-						  NET_SOCK_STREAM, NET_IPPROTO_TLS_1_2);
+						  type, NET_IPPROTO_TLS_1_2);
 	if (client->transport.tls.sock < 0) {
 		return -errno;
 	}