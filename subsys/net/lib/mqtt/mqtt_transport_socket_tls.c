nrfconnect/sdk-zephyr

[nrf noup] net: mqtt: Provide option to enable TLS session caching
Provides an option to enable TLS session caching for an MQTT
client's secure socket.

Signed-off-by: Jan Tore Guggedal <jantore.guggedal@nordicsemi.no>
Signed-off-by: Robert Lubos <robert.lubos@nordicsemi.no>
Signed-off-by: Dominik Ermel <dominik.ermel@nordicsemi.no>
Signed-off-by: Johann Fischer <johann.fischer@nordicsemi.no>
(cherry picked from commit c02b2a3deee3f6bb05ff5b33abd1506ace29ce66)

subsys/net/lib/mqtt/mqtt_transport_socket_tls.c
@@ -78,6 +78,16 @@ int mqtt_client_tls_connect(struct mqtt_client *client)
 		}
 	}
 
+	if (tls_config->session_cache == TLS_SESSION_CACHE_ENABLED) {
+		ret = zsock_setsockopt(client->transport.tls.sock, SOL_TLS,
+				       TLS_SESSION_CACHE,
+				       &tls_config->session_cache,
+				       sizeof(tls_config->session_cache));
+		if (ret < 0) {
+			goto error;
+		}
+	}
+
 	if (tls_config->cert_nocopy != TLS_CERT_NOCOPY_NONE) {
 		ret = zsock_setsockopt(client->transport.tls.sock, SOL_TLS,
 				       TLS_CERT_NOCOPY, &tls_config->cert_nocopy,

tomi-font/ncs-zephyr

Signed-off-by: Tomasz Moń <tomasz.mon@nordicsemi.no>
(cherry picked from commit 270cc3fbdaf5af33b2e012bad4c22275fe56960c)

subsys/net/lib/mqtt/mqtt_transport_socket_tls.c
@@ -122,6 +122,16 @@ int mqtt_client_tls_connect(struct mqtt_client *client)
 		}
 	}
 
+	if (tls_config->session_cache == ZSOCK_TLS_SESSION_CACHE_ENABLED) {
+		ret = zsock_setsockopt(client->transport.tls.sock, ZSOCK_SOL_TLS,
+				       ZSOCK_TLS_SESSION_CACHE,
+				       &tls_config->session_cache,
+				       sizeof(tls_config->session_cache));
+		if (ret < 0) {
+			goto error;
+		}
+	}
+
 	if (tls_config->cert_nocopy != ZSOCK_TLS_CERT_NOCOPY_NONE) {
 		ret = zsock_setsockopt(client->transport.tls.sock, ZSOCK_SOL_TLS,
 				       ZSOCK_TLS_CERT_NOCOPY, &tls_config->cert_nocopy,